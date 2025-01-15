70b89ff41b1c27a36788b8fa9ddd6be092473c56
Clifong <59185915+Clifong@users.noreply.github.com>
2025-01-15 23:04:28 +0800

    Add support for echo
    Echo function now available

--- src/main/java/Acheron.java
+++ src/main/java/Acheron.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Acheron {
     public static void main(String[] args) {
         String logo = "____________________________________________________________\n"
@@ -7,5 +10,26 @@ public class Acheron {
                 + " Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________";
         System.out.println("Hello from\n" + logo);
+
+        while (true) {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            String input = "";
+            try {
+                input = bufferedReader.readLine();
+            } catch (Exception e) {
+                System.out.println("Something went wrong!");
+                break;
+            }
+
+            String genericText = "____________________________________________________________\n" +
+                                 "%s\n" +
+                                 "____________________________________________________________";
+            if (input.equals("bye")) {
+                System.out.println(genericText.formatted("Bye. Hope to see you again soon!"));
+                break;
+            } else {
+                System.out.println(genericText.formatted(input));
+            }
+        }
     }
 }
