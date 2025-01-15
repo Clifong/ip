b2e3efd135e4bf5f55311a87ab1ca57f7192acb8
Clifong <59185915+Clifong@users.noreply.github.com>
2025-01-15 23:11:40 +0800

    Add support for list

--- src/main/java/Acheron.java
+++ src/main/java/Acheron.java
@@ -1,8 +1,11 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Acheron {
+    private static ArrayList<String> tasks = new ArrayList<>();
     public static void main(String[] args) {
+
         String logo = "____________________________________________________________\n"
                 + " Hello! I'm Acheron\n"
                 + " What can I do for you?\n"
@@ -27,8 +30,19 @@ public class Acheron {
             if (input.equals("bye")) {
                 System.out.println(genericText.formatted("Bye. Hope to see you again soon!"));
                 break;
+            } else if (input.equals("list")) {
+                String listOfTasks = "";
+                for (int i = 0; i < tasks.size(); i++) {
+                    if (i < tasks.size() - 1) {
+                        listOfTasks += String.format("%d. %s\n", i + 1, tasks.get(i));
+                    } else {
+                        listOfTasks += String.format("%d. %s", i + 1, tasks.get(i));
+                    }
+                }
+                System.out.println(genericText.formatted(listOfTasks));
             } else {
-                System.out.println(genericText.formatted(input));
+                System.out.println(genericText.formatted("added: " + input));
+                tasks.add(input);
             }
         }
     }
